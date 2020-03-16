5641a0ae9d348e656a71b6e9934a81baea75c374
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-03-16 08:57:13 -0500

    Update live workshop

--- src/Tarea.jsx
+++ src/Tarea.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const Tarea = (props) => {
+    const [tachado, cambiarTachado] = React.useState(false)
+
+    let liClassname = "tareita";
+
+    if(tachado) {
+        liClassname += " tareita-tachada"
+    }
+
     return (
-        <li className="tareita">
+        <li className={liClassname}>
             <input type="checkbox"
                 onChange={
-
                     (e)=> {
-                        console.log(e.target.checked);
+                        cambiarTachado(e.target.checked)
                     }
                 }
             />
